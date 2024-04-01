7170c5b650d93ecbee2398ea998c895075031e27
woogieon8on <swp0927@naver.com>
2024-04-01 21:59:36 +0900

    동빈이의 큰 수의 법칙 수정
    sh: line 1: q: command not found

--- woogieon8on/chap3/3-2.py
+++ woogieon8on/chap3/3-2.py
@@ -2,17 +2,15 @@ n, m, k = map(int, input().split())
 num = list(map(int, input().split()))
 
 num.sort() # 입력받은 수 정렬
-big1 = num[n-1] # 첫 번째로 큰 수
+big1 = num[n-1] # 가장 큰 수
 big2 = num[n-2] # 두 번째로 큰 수
 
-sum = 0
-count = 0
+# 가장 큰 수가 더해지는 횟수 계산
+count = int(m / (k+1)) * k
+count += m % (k+1)
 
-while count < m: # 숫자가 더해지는 횟수 m
-    for i in range(k): # 첫 번째로 큰 수 k번 덧셈
-        sum += big1
-        count += 1
-    sum += big2 # 두 번째로 큰 수 덧셈
-    count += 1
+result = 0
+result += (count) * big1 # 가장 큰 수 덧셈
+result += (m-count) * big2 # 두 번째로 큰 수 덧셈
 
-print(sum)
+print(result)
