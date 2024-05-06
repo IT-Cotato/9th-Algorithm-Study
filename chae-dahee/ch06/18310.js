f4eef7528f2b47666898057d4c0bbe46b60223db
dah <cdh010126r@gmail.com>
2024-05-06 23:15:55 +0900

    [BOJ] 안테나 / 6장 / 30분 / 실버3

--- chae-dahee/ch06/18310.js
+++ chae-dahee/ch06/18310.js
@@ -4,8 +4,12 @@ const n = parseInt(input[0]);
 const house = input[1].split(" ").map(Number)
 
 function Antenna(house){
-    house.sort((a, b) => b-a);
-    return  position = house[Math.floor(house.length / 2)];
+    // house.sort((a, b) => b-a);
+    // return  position = house[Math.floor(house.length / 2)];
+
+    house.sort((a, b) => a-b);
+    const  position = Math.floor(house.length / 2);
+    return house.length % 2 === 0? house[position-1] : house[position];
 }
 
 console.log(Antenna(house));
