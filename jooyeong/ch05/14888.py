786b0c7aa71fc692ca8268832e9258119fc9f18b
JooYeong Lee <0x14@daum.net>
2024-05-07 00:02:50 +0900

    JooYeong Lee / 6주차 / 6장 (#24)
    * [BOJ] 연산자 끼워넣기 / 5장 / 50분 /  실버1
    
    https://www.acmicpc.net/problem/14888
    
    * [BOJ] 특정 거리의 도시 찾기 / 5장 / 50분 / 실버2
    
    https://www.acmicpc.net/problem/18352
    
    * 5주차 코드 일부 수정
    
    * [BOJ] 국영수 / 6장 / 20분 / 실버4
    
    https://www.acmicpc.net/problem/10825
    
    * [BOJ] 안테나 / 6장 / 30분 / 실버3
    
    https://www.acmicpc.net/problem/18310

--- jooyeong/ch05/14888.py
+++ jooyeong/ch05/14888.py
@@ -1,5 +1,7 @@
 # DFS로 풀이
 
+import sys
+
 n = int(input()) # 개수
 num = list(map(int, input().split())) # 수열을 리스트로 저장
 plus, minus, multi, div = map(int, input().split()) # 연산자별 개수
@@ -10,7 +12,7 @@ minimum = 1e9
 def dfs(i, temp):
     global maximum, minimum, plus, minus, multi, div
 
-    if i == n-1 : # 모든 노드를 탐색했을 경우
+    if i == n : # 모든 노드를 탐색했을 경우
         maximum = max(maximum, temp)
         minimum = min(minimum, temp)
 
@@ -21,26 +23,26 @@ def dfs(i, temp):
     # (연산자 개수를 +1 하지 않으면 처음 계산한 값이 그대로 최댓값, 최솟값으로 출력되며 실행이 종료됨.)
 
     if plus > 0 :
-        plus -= 1 # 1 1 1 1
-        dfs(i+1, temp + num[i+1])
+        plus -= 1
+        dfs(i+1, temp + num[i])
         plus += 1
 
     if minus > 0 :
         minus -= 1
-        dfs(i+1, temp - num[i+1])
+        dfs(i+1, temp - num[i])
         minus += 1
 
     if multi > 0 :
         multi -= 1
-        dfs(i+1, temp * num[i+1])
+        dfs(i+1, temp * num[i])
         multi += 1
 
     if div > 0 :
         div -= 1
-        dfs(i+1, int(temp / num[i+1]))
+        dfs(i+1, int(temp / num[i]))
         div += 1
 
-dfs(0, num[0])
+dfs(1, num[0])
 
 print(maximum)
 print(minimum)
