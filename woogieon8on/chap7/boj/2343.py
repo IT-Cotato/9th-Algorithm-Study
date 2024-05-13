be50547d127e9527c16560686c90cb55be22bf37
woogieon8on <swp0927@naver.com>
2024-05-13 17:59:56 +0900

    [BOJ] 기타 레슨 / 7장 / 50분 / 실버1

---
+++ woogieon8on/chap7/boj/2343.py
@@ -0,0 +1,26 @@
+n, m = map(int, input().split())
+lectures = list(map(int, input().split()))
+
+# 블루레이 크기를 이진탐색의 초기 범위로 지정
+start = max(lectures) # 모든 강의를 각각 블루레이로 녹화할 경우 블루레이의 크기 최소 -> 길이가 가장 긴 강의의 길이
+end = sum(lectures) # 모든 강의를 한 번에 블루레이로 녹화할 경우 블루레이의 크기 최대 -> 모든 강의 길이의 합
+
+result = 0
+# 이진 탐색
+while(start <= end):
+    total = 0
+    bluray = 1
+    mid = (start + end) // 2
+    for lec in lectures:
+        if total + lec > mid: # 강의 길이의 총 합이 mid보다 큰 경우
+            bluray += 1 # 블루레이 개수 1 증가
+            total = 0 # 강의 길의의 총 합 초기화
+        total += lec
+
+    if bluray <= m: # 블루레이 개수가 원하는 개수보다 작거나 같은 경우
+        result = mid
+        end = mid - 1 # 블루레이 개수 늘리기 위해 mid값 감소
+    else:
+        start = mid + 1 # 블루레이 개수 줄이기 위해 mid값 증가
+
+print(result)
