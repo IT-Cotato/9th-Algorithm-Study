fe91953170dfdd6f00486242061e0463305372d5
chae-dahee <96566750+chae-dahee@users.noreply.github.com>
2024-05-17 14:42:33 +0900

    chae-dahee / 7주차 / 7장 (#27)
    * 1이 될 때까지 / 3장 / 15분
    
    * [BOJ] NBA 농구 / 4장 / 1시간 / 실버3
    
    * [BOJ] 올림픽 / 4장 / 1시간 / 실버5
    
    * [BOJ] 랭킹전 대기열 / 4장 / 1시간 / 실버1
    
    * [교재] 왕실의 나이트 / 4장 / 20분
    
    * [교재] 게임 개발 / 4장 / 30분
    
    * [BOJ] 특정 거리의 도시 찾기 / 5장 / 1시간 / 실버2
    
    * [BOJ] 연산자 끼워넣기 / 5장 / 50분 / 실버1
    
    * [BOJ] 국영수 / 6장 / 20분 / 실버4
    
    * [BOJ] 안테나 / 6장 / 20분 / 실버3
    
    * [BOJ] 카드 정렬하기 / 6장 / 1시간 / 골드4
    
    * [BOJ] 안테나 / 6장 / 20분 / 실버3
    
    * [BOJ] 안테나 / 6장 / 30분 / 실버3
    
    * [BOJ] 나무 자르기 / 7장 / 30분 / 실버2

---
+++ chae-dahee/ch07/2805.js
@@ -0,0 +1,25 @@
+let input = require("fs").readFileSync("/dev/stdin").toString().trim().split("\n");
+
+const [N, M] = input.shift().split(" ");
+arr = input[0].split(" ").map(Number).sort((a, b) => a - b);
+
+let start = 0;
+let end = arr[arr.length - 1];
+let ans = Number.MIN_SAFE_INTEGER;
+
+while (start <= end) {
+  let mid = Math.floor((start + end) / 2);
+  let sum = 0;
+
+  for (let x of arr) {
+    if (x > mid) sum += x - mid;
+  }
+
+  //sum > M 길경우 mid+1
+  if (sum >= M) {
+    if (mid > ans) ans = mid;
+    start = mid + 1;
+  } else end = mid - 1;
+}
+
+console.log(ans);
