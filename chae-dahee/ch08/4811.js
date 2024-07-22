e586fe520d794784de3ca552b994c6b3fa19a6a5
dah <cdh010126r@gmail.com>
2024-07-22 22:19:38 +0900

    [BOJ] 알약 / 8장 / 60분 / 골드5

---
+++ chae-dahee/ch08/4811.js
@@ -0,0 +1,27 @@
+const pill = require("fs")
+  .readFileSync("./dev/stdin")
+  .toString()
+  .trim()
+  .split("\n")
+  .map(Number);
+pill.pop();
+
+const MAX = Math.max(...pill);
+const dp = new Array(MAX + 1).fill(0);
+
+dp[0] = 1;
+dp[1] = 1;
+dp[2] = 2;
+
+for (let i = 3; i <= MAX; i++) {
+  for (let j = 0; j < i; j++) {
+    dp[i] += dp[j] * dp[i - j - 1];
+  }
+}
+
+const answer = [];
+pill.forEach((v) => {
+  answer.push(dp[v]);
+});
+
+console.log(answer.join("\n"));
