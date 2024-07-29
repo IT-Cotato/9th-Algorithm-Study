5eda244484e4114615477e37c09fdf4d9e88ef56
woogieon8on <swp0927@naver.com>
2024-07-29 18:23:54 +0900

    [BOJ] 경로 찾기 / 9장 / 40분 / 실버 1

--- woogieon8on/chap9/boj/2606.py
+++ woogieon8on/chap9/boj/2606.py
@@ -14,16 +14,16 @@ for _ in range(e):
 
 
 def bfs(x):
-    deq = deque([x]) # x: 시작점
+    queue = deque([x]) # x: 시작점
     count = 0
     visited[x] = True # 시작점 방문 표시
 
-    while deq:
-        node = deq.popleft()
+    while queue:
+        node = queue.popleft()
         for next_node in graph[node]: # 현재 컴퓨터에 연결된 모든 컴퓨터
             if not visited[next_node]: # 방문하지 않은 컴퓨터인 경우
                 visited[next_node] = True # 방문 표시
-                deq.append(next_node) # 큐에 추가
+                queue.append(next_node) # 큐에 추가
                 count += 1 # 감염된 컴퓨터 수 증가
 
     return count
